518ea1967c4de73467e759211e11e3c02a97311e
pashuh <pashuh08@yandex.ru>
2022-07-11 13:02:51 +0300

    add all

---
+++ src/test/java/steps/StepsForSberTech.java
@@ -0,0 +1,136 @@
+package steps;
+
+import com.codeborne.selenide.Condition;
+import io.qameta.allure.Step;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class StepsForSberTech {
+    @Step("Открываем страницу SberTech")
+    public void openPage() {
+        open("https://sbertech.ru/");
+        $(".min-h-screen.flex.flex-col").shouldHave(text("СберТех"));
+    }
+
+    @Step("Кликнуть в хедере 'Вакансии'")
+    public void clickVacancies() {
+        $(".flex.justify-between.align-baseline.w-full").$("[target=_blank]").click();
+        sleep(7000);
+    }
+
+    @Step("Перейти на открывшуюся вкладку в браузере")
+    public void goswitchTo(int number) {
+        switchTo().window(number);
+    }
+
+    @Step("Ввести QA в поле 'Какую работу ищешь'")
+    public void setValueVacansies(String vacancies) {
+        $("[placeholder='Какую работу ищешь?']").setValue(vacancies);
+    }
+
+    @Step("Кликнуть 'Найти'")
+    public void clickSearch() {
+        $("[type=submit]").click();
+//        sleep(7000);
+    }
+
+    @Step("Проверяем наличие искомых вакансий")
+    public void findVac (String vacancies) {
+        $$(".css-cqzo9v.eotq4q00").find(Condition.text(vacancies)).shouldBe(Condition.visible);
+    }
+
+    @Step("Проставляем/отключаем вакансии ПАО Сбербанк")
+    public void clickPAO(){
+        $(".css-1qh51lz.eotq4q00").$(byText("ПАО Сбербанк")).click();
+    }
+
+    @Step("Проставляем/отключаем вакансии АО Сбербанк-Технологии")
+    public void clickAO() {
+        $(".css-1qh51lz.eotq4q00").$(byText("АО Сбербанк-Технологии")).click();
+    }
+
+    @Step("Вводим город для поиска вакансий")
+    public void setValueCityMoscow(){
+        $("[placeholder='Населенный пункт']").setValue("Москва");
+        sleep(7000);
+        $("[role=presentation]").$(byText("Город Москва")).click();
+    }
+
+    @Step("Проверяем в найденных вакансиях выбранный город")
+    public void findCity() {
+        $$(".css-elfudn").find(Condition.text("Город Москва")).shouldHave(Condition.visible);
+    }
+
+    @Step("Проверяем в найденных вакансиях компанию")
+    public void findCompany() {
+        $$(".css-elfudn").find(Condition.text("ПАО Сбербанк")).shouldHave(Condition.visible);
+    }
+
+    @Step("Переходим к программам в вузах")
+    public void goToPrograms(){
+        $(".flex.justify-between.align-baseline.w-full").$("[href='/streams']").click();
+    }
+
+    @Step("Открываем Школу 21")
+    public void openSchool21() {
+        $("[href='https://21-school.ru']").click();
+    }
+
+    @Step("Открываем направления в Школе 21")
+    public void learning() {
+        $(".header__inner").$("[data-goto='.learning']").click();
+    }
+
+    @Step("Проверяем наличие направления DevOps")
+    public void devOps() {
+        $(".learning__wrapper").$(byText("DevOps")).shouldHave(Condition.text("DevOps"));
+    }
+
+    @Step("Открываем Sberseasons")
+    public void openSberseasons() {
+        $("[href='https://sbergraduate.ru/sberseasons-moscow/']").click();
+    }
+
+    @Step("Заполняем заявку личными данными")
+    public void setFIO() {
+        $(".hero-sberseasons").$("[name='surname']").setValue("Тест");
+        $(".hero-sberseasons").$("[name='name']").setValue("Один");
+        $(".hero-sberseasons").$("[name='patronymic']").setValue("Два");
+    }
+
+    @Step("Вводим Email")
+    public void setMail() {
+        $(".hero-sberseasons").$("[name='email']").setValue("test@mail.com");
+    }
+
+    @Step("Выбираем город Омск")
+    public void setCityOmsk() {
+        $(".hero-sberseasons").$(".select__item.is-empty").click();
+        $("#select__popup").$(byText("Омск")).click();
+//       sleep(2000);
+    }
+
+    @Step("Вводим номер телефона")
+    public void setPhone() {
+        $(".hero-sberseasons").$("[name='phone']").setValue("79993331122");
+    }
+
+    @Step("Переходим к командам SberTech")
+    public void commandsSberTech() {
+        $(".flex.justify-between.align-baseline.w-full").$("[href='/team']").click();
+    }
+
+    @Step("Выбираем команду Департамент образовательных цифровых платформ")
+    public void depEducation() {
+        $(byText("Показать еще")).click();
+        $(".justify-start").$(byText("Департамент образовательных цифровых платформ")).click();
+    }
+
+    @Step("Проверяем наличие команды Департамент образовательных цифровых платформ")
+    public void finddepEducation() {
+        $(".text-h4").shouldHave(Condition.text("Департамент образовательных цифровых платформ"));
+    }
+
+}
---
+++ src/test/java/tests/SberTech.java
@@ -0,0 +1,98 @@
+package tests;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import helpers.Attach;
+import io.qameta.allure.Link;
+import io.qameta.allure.Owner;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import steps.StepsForSberTech;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+@Owner("pashuh")
+@Link(name = "SberTech", url = "https://sbertech.ru/")
+public class SberTech extends TestBase {
+    StepsForSberTech steps = new StepsForSberTech();
+    String vacancies = "QA";
+    @Test
+    @Tag("job_search")
+    @DisplayName("Поиск вакансий QA")
+    public void sberTechTest1() {
+//        Configuration.browserSize = "1920x1080";
+//        Configuration.timeout = 7000;
+        steps.openPage();
+        steps.clickVacancies();
+        steps.goswitchTo(1);
+        steps.setValueVacansies(vacancies);
+        steps.clickSearch();
+        steps.findVac(vacancies);
+    }
+
+    @Test
+    @Tag("job_search")
+    @DisplayName("Поиск вакансий QA в ПАО Сбербанк в г. Москва")
+    public void sberTechTest2() {
+//        Configuration.browserSize = "1920x1080";
+//        Configuration.timeout = 7000;
+        steps.openPage();
+        steps.clickVacancies();
+        steps.goswitchTo(2);
+        steps.clickPAO();
+        steps.clickAO();
+        steps.setValueVacansies(vacancies);
+        steps.setValueCityMoscow();
+        steps.clickSearch();
+        steps.findVac(vacancies);
+        steps.findCity();
+        steps.findCompany();
+    }
+
+    @Test
+    @Tag("viewing_programs")
+    @DisplayName("Проверка наличия направления DevOps в Школе 21")
+    public void sberTechTest3() {
+        steps.openPage();
+        steps.goToPrograms();
+        steps.openSchool21();
+        steps.goswitchTo(3);
+        steps.learning();
+        steps.devOps();
+    }
+
+    @Test
+    @Tag("application")
+    @DisplayName("Заполнение заявки в Sberseasons")
+    public void sberTechTest4() {
+        steps.openPage();
+        steps.goToPrograms();
+        steps.openSberseasons();
+        steps.goswitchTo(4);
+        steps.setFIO();
+        steps.setMail();
+        steps.setCityOmsk();
+        steps.setPhone();
+    }
+
+    @Test
+    @Tag("viewing_commands")
+    @DisplayName("Просмотр команд в SberTech")
+    public void sberTechTest5() {
+        steps.openPage();
+        steps.commandsSberTech();
+        steps.depEducation();
+        steps.finddepEducation();
+    }
+
+//    @AfterEach
+//    void afterEach() {
+//        Attach.screenshotAs("Last screenshot");
+//        Attach.pageSource();
+//        Attach.browserConsoleLogs();
+//        Attach.addVideo();
+//    }
+}
